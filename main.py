65d5152fcaf2b6e38f5c8f778733847b349a21d8
fires5050 <nathan@speck-online.com>
2025-06-13 18:27:43 +0100

    removed hardcoded values and adapted the script to take the argument of the file location to analyse. 'Python3 main.py <path_to_book>'

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if (len(sys.argv)) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 from stats import get_word_count
 from stats import get_char_count
 from stats import sort_dict
@@ -6,15 +12,15 @@ def get_book_text(path_to_file):
                 file_contents = f.read()
         return file_contents
 def main():
-        num_words = get_word_count(get_book_text("books/frankenstein.txt"))
-        num_chars = get_char_count(get_book_text("books/frankenstein.txt"))
+        num_words = get_word_count(get_book_text(sys.argv[1]))
+        num_chars = get_char_count(get_book_text(sys.argv[1]))
         ordered_list = sort_dict(num_chars)
         #print (f"{num_words} words found in the document")
         #for character in num_chars:
         #        number = num_chars[character]
         #        print (f"'{character}': {number}")
         print ("============ BOOKBOT ============")
-        print ("Analyzing book found at books/frankenstein.txt...")
+        print (f"Analyzing book found at {sys.argv[1]}")
         print ("----------- Word Count ----------")
         print (f"Found {num_words} total words")
         print ("--------- Character Count -------")
